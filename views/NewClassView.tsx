d7f15c174e152b21a212eea9fdf1da07df9afdcb
João Guerra <joaorochaguerra@ua.pt>
2026-01-03 17:02:58 +0000

    Updated NewClassView.tsx

--- views/NewClassView.tsx
+++ views/NewClassView.tsx
@@ -22,13 +22,29 @@ const NewClassView: React.FC<Props> = ({ onBack }) => {
   const [catechistSearch, setCatechistSearch] = useState(''); 
   const currentUser = backend.getCurrentUser();
 
+  const loadCatechists = async () => {
+    const users = await backend.listUsersForAssignment();
+    setAvailableCatechists(users);
+  };
+
   useEffect(() => {
-    backend.listUsersForAssignment().then(users => {
-      setAvailableCatechists(users);
-      if (currentUser) {
-        setSelectedCatechistIds([currentUser.id]);
-      }
-    });
+    loadCatechists();
+    if (currentUser) {
+      setSelectedCatechistIds([currentUser.id]);
+    }
+
+    const interval = setInterval(loadCatechists, 2000);
+
+    const handleFocus = () => {
+      loadCatechists();
+    };
+
+    window.addEventListener('focus', handleFocus);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('focus', handleFocus);
+    };
   }, []);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
